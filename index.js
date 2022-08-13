fe29ec5429931bacb68da10364b9ddeb8aebd720
principles31 <principles31@gmail.com>
2022-08-13 15:47:42 +0000

    Add js file for manupuliting

---
+++ index.js
@@ -0,0 +1,15 @@
+let saveEL = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEL.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
