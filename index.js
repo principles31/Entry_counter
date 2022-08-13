df5118037631ea1b37368eb0a2abeb2f4d7eb97e
principles31 <principles31@gmail.com>
2022-08-13 16:15:46 +0000

    Fix error

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const countEl = document.getElementById('count-el');
 const count = 0;
 
 function increment() {
-  count += 1
+  count += 1;
   countEl.textContent = count;
 }
 
 function save() {
-  const countStr = count + " - "
+  const countStr = count + ' - ';
   saveEL.textContent += countStr;
   countEl.textContent = 0;
   count = 0;
