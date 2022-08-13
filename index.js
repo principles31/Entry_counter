1931968891022a86f9bf8690045f339650540688
principles31 <principles31@gmail.com>
2022-08-13 16:04:47 +0000

    Update files

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let saveEL = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+const saveEL = document.getElementById('save-el');
+const countEl = document.getElementById('count-el');
+const count = 0;
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count += 1
+  countEl.textContent = count;
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEL.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+  const countStr = count + " - "
+  saveEL.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
